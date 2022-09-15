1868f8d0a75efd002ff77408f71f983d857a3a28
Woojin Lee <86906199+nijoow@users.noreply.github.com>
2022-09-16 01:08:44 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 156 ms, Memory: 10024 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.js
@@ -0,0 +1,30 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+readline
+  .on("line", function (line) {
+    input.push(line);
+  })
+  .on("close", function () {
+    solution(input);
+    process.exit();
+  });
+
+const solution = (input) => {
+  input = input.map((line) => line.split(" ").map((val) => Number(val)));
+  const testCase = input.filter((_, index) => index !== 0);
+  testCase.forEach((value) => {
+    const num = value[1] - value[0];
+    if (Math.sqrt(num) % 1 === 0) console.log(2 * Math.sqrt(num) - 1);
+    else {
+      (Math.floor(Math.sqrt(num)) ** 2 + Math.ceil(Math.sqrt(num)) ** 2) / 2 <=
+      num
+        ? console.log(2 * Math.ceil(Math.sqrt(num)) - 1)
+        : console.log(2 * Math.ceil(Math.sqrt(num)) - 2);
+    }
+  });
+};
