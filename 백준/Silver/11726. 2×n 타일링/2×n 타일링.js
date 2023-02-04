a9d5878ad9a504f9e709473aee5946684d5f61af
Woojin Lee <86906199+nijoow@users.noreply.github.com>
2023-02-04 12:21:15 +0900

    [Silver III] Title: 2×n 타일링, Time: 276 ms, Memory: 9780 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -0,0 +1,25 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let n = null;
+
+readline
+  .on("line", function (line) {
+    n = +line;
+  })
+  .on("close", function () {
+    solution(n);
+    process.exit();
+  });
+
+const solution = (n) => {
+  const dp = Array.from(Array(n + 1), () => 0);
+  dp[1] = 1;
+  dp[2] = 2;
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+  }
+  console.log(dp[n]);
+};
