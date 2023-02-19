48e0c44d5304c26c08f8800a8f4d7b10628e34ee
Woojin Lee <86906199+nijoow@users.noreply.github.com>
2023-02-19 14:10:50 +0900

    [Silver II] Title: DFS와 BFS, Time: 272 ms, Memory: 25832 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
@@ -3,78 +3,60 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 });
 
-let input = [];
+let N = null,
+  M = null,
+  V = null,
+  input = [];
 
 readline
   .on("line", function (line) {
-    input.push(line);
+    if (!N) [M, N, V] = line.split(" ").map((el) => +el);
+    else {
+      input.push(line.split(" ").map((el) => +el));
+    }
   })
   .on("close", function () {
-    solution(input);
+    solution(M, N, V, input);
     process.exit();
   });
 
-const solution = (input) => {
-  input = input.map((line) => line.split(" ").map((val) => Number(val)));
-  const [N, M, V] = [input[0][0], input[0][1], input[0][2]];
-  const node = [];
-  let dfs = "";
-  let bfs = "";
-  input.forEach((val, index) => {
-    if (index !== 0) {
-      node.push(val);
-      const reverse = [...val].reverse();
-      node.push(reverse);
+const solution = (M, N, V, input) => {
+  const graph = Array.from(Array(M + 1), () => Array(M + 1).fill(0));
+  input.forEach((item) => {
+    graph[item[0]][item[1]] = 1;
+    graph[item[1]][item[0]] = 1;
+  });
+  const visitedDFS = Array(M + 1).fill(false);
+  const resultDFS = [];
+  const DFS = (now) => {
+    resultDFS.push(now);
+    for (let i = 1; i <= M; i++) {
+      if (graph[now][i] === 1 && !visitedDFS[i]) {
+        visitedDFS[i] = true;
+        DFS(i);
+      }
     }
-  });
-  node.sort((a, b) => {
-    if (a[0] === b[0]) return a[1] - b[1];
-    return a[0] - b[0];
-  });
-  const visited = Array(N + 1).fill(0);
-  visited[V] = 1;
-  DFS(0, V);
-  BFS();
-  console.log(dfs);
-  console.log(bfs);
+  };
+  visitedDFS[V] = true;
+  DFS(V);
 
-  function DFS(L, num) {
-    dfs += num;
-    if (L === N) {
-      return;
-    } else {
-      for (let i = 0; i < node.length; i++) {
-        const value = node[i];
-        if (value[0] === num && visited[value[1]] === 0) {
-          visited[value[1]] = 1;
-          dfs += " ";
-          DFS(L + 1, value[1]);
-        }
+  const visitedBFS = Array(M + 1).fill(false);
+  const resultBFS = [];
+
+  visitedBFS[V] = true;
+  const queue = [V];
+
+  while (queue.length > 0) {
+    const now = queue.shift();
+    resultBFS.push(now);
+
+    for (let i = 1; i <= M; i++) {
+      if (graph[now][i] === 1 && !visitedBFS[i]) {
+        visitedBFS[i] = true;
+        queue.push(i);
       }
     }
   }
-
-  function BFS() {
-    const visitedBfs = Array(N + 1).fill(0);
-    const queue = [];
-    for (let i = 0; i < node.length; i++) {
-      if (node[i][0] === V) {
-        queue.push(node[i]);
-      }
-    }
-    visitedBfs[V] = 1;
-    bfs += V;
-    while (queue.length > 0) {
-      const item = queue.shift();
-      if (visitedBfs[item[1]] === 0) {
-        visitedBfs[[item[1]]] = 1;
-        bfs += ` ${item[1]}`;
-      }
-      for (let i = 0; i < node.length; i++) {
-        if (node[i][0] === item[1] && visitedBfs[node[i][1]] === 0) {
-          queue.push(node[i]);
-        }
-      }
-    }
-  }
+  console.log(resultDFS.join(" "));
+  console.log(resultBFS.join(" "));
 };
