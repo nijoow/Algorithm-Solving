704eba715c1eeb840b33142219342ab6fb15508e
Woojin Lee <86906199+nijoow@users.noreply.github.com>
2024-02-15 23:40:00 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.js
@@ -0,0 +1,28 @@
+function solution(edges) {
+    const max = edges.reduce((max,[a,b])=>Math.max(max,a,b),1)
+    const outArr = Array.from(Array(max+1),()=>0)
+    const inArr = Array.from(Array(max+1),()=>0)
+
+    edges.forEach(([a,b])=>{
+        outArr[a]++
+        inArr[b]++
+    })
+
+    let start = null
+    let graph = {donut:0,bar:0,eight:0}
+    
+    for(let i=1;i<=max;i++){
+        if(inArr[i] === 0 && outArr[i] >= 2){
+            start = i
+        }
+        if(inArr[i] >= 1 && outArr[i] === 0){
+            graph.bar++
+        }
+        if(inArr[i] >= 2 && outArr[i] >= 2){
+            graph.eight++
+        }
+    }
+    graph.donut = outArr[start] - graph.bar - graph.eight
+
+    return [start, graph.donut, graph.bar, graph.eight]
+}
